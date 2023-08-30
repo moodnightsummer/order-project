f82b3f393b7e7f9247a3ca3bcc59ef38d41fe85e
jelly <moodnightsummer@naver.com>
2023-08-30 21:33:39 +0900

    fix : database 수정

--- src/database/entities/User.ts
+++ src/database/entities/User.ts
@@ -1,11 +1,24 @@
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
 import { Order } from "./Order";
 import { Payment } from "./Payment";
+import { Store } from "./Store";
 
+@Index("user_FK", ["storeSeq"], {})
 @Entity("user", { schema: "torder" })
 export class User {
   @PrimaryGeneratedColumn({ type: "int", name: "seq", comment: "유저 번호" })
   seq: number;
+
+  @Column("int", { name: "store_seq", comment: "매장 번호" })
+  storeSeq: number;
 
   @Column("varchar", { name: "id", comment: "아이디", length: 30 })
   id: string;
@@ -21,4 +34,11 @@ export class User {
 
   @OneToMany(() => Payment, (payment) => payment.userSeq2)
   payments: Payment[];
+
+  @ManyToOne(() => Store, (store) => store.users, {
+    onDelete: "NO ACTION",
+    onUpdate: "NO ACTION",
+  })
+  @JoinColumn([{ name: "store_seq", referencedColumnName: "seq" }])
+  storeSeq2: Store;
 }
