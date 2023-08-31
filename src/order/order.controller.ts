5b66c2f19a721ed7f0c29997ff597710c4ecdcb5
jelly <moodnightsummer@naver.com>
2023-08-31 15:35:14 +0900

    fix : 주문 기능 수정

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CustomResponseDto } from 'src/dto/customResponseDto';
 import { AccessTokenGuard } from 'src/auth/accessToken.guard';
@@ -17,21 +17,13 @@ export class OrderController {
     @Req() req,
     @Body() body: OrderDto,
   ): Promise<CustomResponseDto> {
-    try {
-      // 기존 주문 내역이 있는지 확인
-      const orderStatus = await this.orderService.checkExistOrder(req.user.seq);
+    // 기존 주문 내역이 있는지 확인
+    const orderStatus = await this.orderService.checkExistOrder(req.user.seq);
 
-      return await this.orderService.order(
-        req.user.seq,
-        orderStatus,
-        body.orderList,
-      );
-
-      // 주문 내역이 있을 시 현재 주문에 주문 내역 추가
-
-      return new CustomResponseDto(200, { message: '성공' });
-    } catch (error) {
-      return new CustomResponseDto(403, { message: '에러' });
-    }
+    return await this.orderService.order(
+      req.user.seq,
+      orderStatus,
+      body.orderList,
+    );
   }
 }
